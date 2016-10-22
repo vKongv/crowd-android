543aaf516723031d2a464aa0dcef3b84430bb20b
vKongv <kaweng456@hotmail.com>
2016-10-22 23:30:45 +0800

    -Added library `easyFoursquare4Android`

--- app/src/main/java/com/example/vkongv/crowd/MainActivity.java
+++ app/src/main/java/com/example/vkongv/crowd/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.vkongv.crowd;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.com.condesales.EasyFoursquare;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EasyFoursquare test = new EasyFoursquare(this);
     }
 }
